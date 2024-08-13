2e230a6b9e72723e95bdecd34956a6a42bff062d
Shaikhakreatr <shaikha@thekreatr.com>
2024-08-13 15:38:17 +0530

    done changs in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: '/:path*'
+      },
+      {
+        source: '/community/:path*',
+        destination: '/community/:path*'
+      },
+    ];
+  },
+  
+  output:"standalone",
+  basePath:"/community",
+  assetPrefix:"/community",
+
     images: {
         domains: ['kreatrwebsite.s3.ap-south-1.amazonaws.com'],
         remotePatterns: [
