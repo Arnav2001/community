f63994b222b7ed486d66bef0a787268e49574017
Arnav Kulshrestha <arnavkul07@gmail.com>
2024-08-13 20:44:48 +0530

    config update

--- next.config.mjs
+++ next.config.mjs
@@ -3,39 +3,32 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/:path*',
-        destination: '/:path*'
-      },
-      {
         source: '/community/:path*',
         destination: '/community/:path*'
       },
+      // Add other rewrites if necessary
     ];
   },
-  
-  output:"standalone",
-  basePath:"/community",
-  assetPrefix:"/community",
-
-    images: {
-        domains: ['kreatrwebsite.s3.ap-south-1.amazonaws.com'],
-        remotePatterns: [
-          {
-            protocol: 'http',
-            hostname: '**',
-            port: '',
-            pathname: '/**',
-          },
-          {
-            protocol: 'https',
-            hostname: '**',
-            port: '',
-            pathname: '/**',
-          },
-        ],
+  output: "standalone",
+  basePath: "/community",
+  assetPrefix: "/community",
+  images: {
+    domains: ['kreatrwebsite.s3.ap-south-1.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '**',
+        port: '',
+        pathname: '/**',
       },
-
+      {
+        protocol: 'https',
+        hostname: '**',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
-
