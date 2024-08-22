2784bf286e4298ba90e27f3d61e23fee9b9ade50
Arnav Kulshrestha <arnavkul07@gmail.com>
2024-08-22 12:52:17 +0530

    google analytics added

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@
 import "./globals.css";
 import '@mantine/core/styles.css';
 import ClientProviders from './ClientProvider';
+import Script from "next/script";
 
 
 export const metadata = {
@@ -14,6 +15,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="gtm"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NS8SNVHC');`,
+          }}
+        ></Script>
+      </head>
+      
       <body  className='bg-img md:min-h-[100vh] xl:min-h-[100vh]'> 
         <ClientProviders>
         {children}     
